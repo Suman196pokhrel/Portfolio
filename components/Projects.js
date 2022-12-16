a8434b5e27719123bc16d486c16065f49a9ad175
Suman Pokhrel <spokhrel196@gmail.com>
2022-12-16 13:19:50 +0545

    Projects section completed

---
+++ components/Projects.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+const Projects = () => {
+
+    const projects = [1, 2, 3, 4, 5, 6]
+
+    return (
+        <motion.div
+        initial={{opacity:0}}
+        whileInView={{opacity:1}}
+        transition={{duration:1.5}}
+            className='h-screen flex relative flex-col md:flex-row overflow-hidden text-left max-w-full justify-evenly
+        mx-auto items-center z-0'
+        >
+
+            <h3 className='absolute top-24 text-gray-500 uppercase tracking-[20px] text-2xl'>Projects</h3>
+
+
+            <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
+                {/* Each Projects  */}
+                {projects.map((project, i) => (
+                    <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center 
+                        justify-center p-20 md:p-44 h-screen'>
+                        <motion.img
+                            initial={{y:-300,opacity:0}}
+                            transition={{duration:1.2}}
+                            whileInView={{opacity:1,y:0}}
+                            viewport={{once:true}}
+                            src="https://th.bing.com/th/id/OIP.-NHfsnk5mf2tjwKlJvBpEQHaEo?pid=ImgDet&rs=1"
+                            alt="smthing"
+                        />
+                        <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
+
+                            <h4 className='text-4xl font-semibold text-center'>
+                                <span className='underline decoration-[#F7AB0A]'>
+                                    Case Study {i + 1} of {projects.length}:
+                                </span>Calculator App
+                            </h4>
+
+                            <p className='text-lg text-center md:text-left'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim est maiores adipisci, harum dolore fuga eligendi vitae corporis earum? Labore suscipit magni corrupti! Laborum, optio dicta ut reprehenderit possimus, eius iste odio placeat modi tenetur saepe in voluptatem molestiae exercitationem dolore doloribus totam cupiditate repudiandae reprehenderit, saepe praesentium?
+                            </p>
+                        </div>
+                    </div>
+                ))}
+
+
+            </div>
+
+
+
+            <div className='w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12'>
+            </div>
+
+
+        </motion.div>
+    )
+}
+
+export default Projects
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import About from '../components/About'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import Projects from '../components/Projects'
 import Skills from '../components/Skills'
 import WorkExperience from '../components/WorkExperience'
 
@@ -40,6 +41,9 @@ export default function Home() {
 
 
       {/* Projects  */}
+      <section id='projects' className='snap-start'>
+        <Projects />
+      </section>
 
 
       {/* Contact  */}
